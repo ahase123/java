e561cab918a2226508bf4db2c37d15bc89160b82
ahase123 <ahase123@yahoo.co.jp>
2021-09-16 13:58:34 +0900

    first commit

---
+++ MainRental.java
@@ -0,0 +1,15 @@
+public class MainRental{
+    public static void main(String[] args){
+
+        Movie my_movie = new Movie("七人の侍",0);
+
+        Customer my_customer = new Customer(hiroko");
+        Rental my_rental = new Rental(my_movie, 10);
+
+        my_customer.addRental(my_rental);
+
+        System.out.println(my_customer.statement());
+
+    }
+    
+}

dbaa2052d6871e155892d5a51642f15626f92182
ahase123 <ahase123@yahoo.co.jp>
2021-09-17 19:29:52 +0900

    update

--- MainRental.java
+++ MainRental.java
@@ -1,26 +1,15 @@
-<<<<<<< HEAD
-// ahase1にて修正
-=======
->>>>>>> origin/master
 public class MainRental{
     public static void main(String[] args){
 
         Movie my_movie = new Movie("七人の侍",0);
 
-<<<<<<< HEAD
-        Customer my_customer = new Customer("hiroko");
-=======
         Customer my_customer = new Customer("akira");
->>>>>>> origin/master
         Rental my_rental = new Rental(my_movie, 10);
 
         my_customer.addRental(my_rental);
 
-<<<<<<< HEAD
-=======
 
 
->>>>>>> origin/master
         System.out.println(my_customer.statement());
 
     }
